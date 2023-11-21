3832babd4659004c846d897e8b9f54e5b5708356
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-11-21 13:38:20 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,7 +2,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 // import { useSelector } from "react-redux";
 // import { selectCars } from "./selectors";
-
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Headers':
+    'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': '*',
+  'X-Requested-With': '*',
+};
 const url = new URL('https://6555fa2384b36e3a431ec238.mockapi.io/advert');
 
 export const fetchCars = createAsyncThunk(
@@ -12,7 +19,7 @@ export const fetchCars = createAsyncThunk(
     try {
       url.searchParams.append('page', 1);
       url.searchParams.append('limit', limit);
-      const response = await axios.get(url);
+      const response = await axios.get(url, headers);
       cars.push(...response.data);
       return cars;
     } catch (e) {
